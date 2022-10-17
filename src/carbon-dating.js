c6924c8d4445058f0e40df01d91bcb9c09d918c6
kankordkoks <konstantinkankord@gmail.com>
2022-10-17 22:50:30 +0400

    feat: modified carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+	let res
+	if (typeof +sampleActivity == 'number' && !isNaN(+sampleActivity) && typeof sampleActivity == 'string' && +sampleActivity > 0) {
+		res = Math.ceil(Math.log(MODERN_ACTIVITY / (+sampleActivity)) / (Math.LN2 / HALF_LIFE_PERIOD))
+	}
+	return res > 0
+		? res
+		: false
 }
+module.exports = {
+	dateSample
+};
 
 module.exports = {
-  dateSample
+	dateSample
 };
