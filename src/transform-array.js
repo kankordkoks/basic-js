8c9621fdabe7780777291df39782030e35faa29f
kankordkoks <konstantinkankord@gmail.com>
2022-10-18 00:51:09 +0400

    feat: modifile file

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,42 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(arr) {
-	// console.log(arr)
-	if (!Array.isArray(arr)) {
-		throw new Error("'arr' parameter must be an instance of the Array!")
+function transform(arr)
+// console.log(arr)
+if (!Array.isArray(arr)) {
+	throw new Error("'arr' parameter must be an instance of the Array!")
+}
+let res = []
+for (let i = 0; i < arr.length; i++) {
+	if (i == 0 && (arr[i] == '--discard-prev'  arr[i] == '--double-prev') ) {
+		i += 1
 	}
-	let res = []
-	for (let i = 0; i < arr.length; i++) {
-		if (i == 0 && (arr[i] == '--discard-prev' arr[i] == '--double-prev')) {
-			i += 1
-		}
-		if (i == arr.length - 1 && (arr[i] == '--double-next' arr[i] == '--discard-next')) {
-			break
-		}
-		if (arr[i] == '--discard-next' && (arr[i + 2] == '--double-prev' || arr[i + 2] == '--discard-prev')) {
-			i += 3
-		}
-		if (arr[i] == '--discard-next' && arr[i + 2] !== '--double-prev' && arr[i + 2] !== '--discard-prev') {
-			i += 2
-		}
-		if (arr[i] == --discard - prev) {
-			res.pop()
-		}
-		if (arr[i] == --double - next) {
-			res.push(arr[i + 1])
-			i += 1
-		}
-		if (arr[i] == --double - prev) {
-			res.push(arr[i - 1])
-			i += 1
-		}
-		if (arr[i] !== --discard - prev) {
-			res.push(arr[i])
-		}
-
+	if (i == arr.length - 1 && (arr[i] == '--double-next'  arr[i] == '--discard-next') ) {
+		break
+	}
+	if (arr[i] == '--discard-next' && (arr[i + 2] == '--double-prev' || arr[i + 2] == '--discard-prev')) {
+		i += 3
+	}
+	if (arr[i] == '--discard-next' && arr[i + 2] !== '--double-prev' && arr[i + 2] !== '--discard-prev') {
+		i += 2
+	}
+	if (arr[i] == --discard - prev) {
+		res.pop()
+	}
+	if (arr[i] == --double - next) {
+		res.push(arr[i + 1])
+		i += 1
+	}
+	if (arr[i] == --double - prev) {
+		res.push(arr[i - 1])
+		i += 1
+	}
+	if (arr[i] !== --discard - prev) {
+		res.push(arr[i])
 	}
 
-	module.exports = {
-		transform
-	};
+}
+
+module.exports = {
+	transform
+};
